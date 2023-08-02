62760441acee9c5b73b2d4da3afda0815c0a1300
ByteNinja42 <dory01check@gmail.com>
2023-08-02 16:46:16 +0200

    repo: added initializing of redis client and config flie

---
+++ config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"os"
+	"strconv"
+)
+
+func getEnv(key, defaultValue string) string {
+	env := os.Getenv(key)
+	if env == "" {
+		env = defaultValue
+	}
+	return env
+}
+
+type RedisConfig struct {
+	Addr     string
+	Password string
+	DB       int
+}
+
+func NewRedisConfig() *RedisConfig {
+	dbNumber, err := strconv.Atoi(getEnv("REDISDB_NUMBER", "0"))
+	if err != nil {
+		dbNumber = 0
+	}
+	return &RedisConfig{
+		Addr:     getEnv("REDIS_ADDR", "localhost:6379"),
+		Password: getEnv("REDIS_PASSWORD", "qwerty"),
+		DB:       dbNumber,
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/ByteNinja42/WeatherTool
 
 go 1.21rc2
+
+require (
+	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
